e43f7d61815e66400de030a1dab17e8d44e1db86
Hajp <sklawik@icloud.com>
2026-07-30 21:55:51 +0200

    compile script fix if file doesnt exist it wouldnt compile

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
 LD_LIBRARY_PATH=. ./pawncc szkodnik-rp2.pwn
-rm ./gamemodes/szkodnik-rp2.amx && mv ./szkodnik-rp2.amx ./gamemodes/szkodnik-rp2.amx
+rm ./gamemodes/szkodnik-rp2.amx 
+mv ./szkodnik-rp2.amx ./gamemodes/szkodnik-rp2.amx
